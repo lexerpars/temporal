b45a43ca818eaf3ca2639bee27fa9ad1e6912dcf
lexerpars <gersonrogelio2@gmail.com>
2017-05-09 03:35:27 +0000

    [REF] Commit Inicial

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "temporal",
+
+    'summary': """
+        Modulo especifico para realizar pruebas de concepto sobre programacion en Odoo""",
+
+    'description': """
+        POC, Proof Of Concept, es un modulo que contiene pruebas de concepto sobre la api que
+        tiene odoo, asi como de diversas tecnicas de programacion.
+    """,
+
+    'author': "rperez",
+
+    'application': True,
+
+    'installable': True,
+
+    'website': "http://www.guatewares.com",
+
+    'category': 'Testing',
+
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+
+    ],
+
+    'demo': [
+
+    ]
+}
